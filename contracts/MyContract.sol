0d8454e4463f78ca9e91addb346288674bfe6155
PyReine <pyreineb@gmail.com>
2024-08-31 17:21:43 +0100

    Add function in MyContract

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -5,6 +5,7 @@ pragma solidity 0.8.14;
 contract MyContract {
 
     string public ourString="Hello World";
-
-
+    function updateOurString(string memory _updateString) public {
+        ourString = _updateString;
+    }
 } 
