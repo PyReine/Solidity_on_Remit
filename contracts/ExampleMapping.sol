8d05c3fac26ae07032812be063084c636c1ab2e2
PyReine <pyreineb@gmail.com>
2024-09-26 11:47:08 +0100

    WalletMapping

---
+++ contracts/ExampleMapping.sol
@@ -0,0 +1,18 @@
+pragma solidity 0.8.15;
+contract ExampleMapping {
+    mapping(uint => bool) public myMapping;
+    mapping(address=> bool) public myAddressMapping;
+    mapping(uint=>mapping(uint=>bool)) public uintUintBoolMapping;
+
+ //   function myMapping(uint _key) public view returns(bool) {
+ //       return myMapping[_key];
+ //   }
+    function setValue(uint _index) public {
+        myMapping[_index] = true;
+    }
+    function setMyAddressToTrue() public {
+        myAddressMapping[msg.sender]=true;
+    }
+    function setUintUintBoolMapping(uint _key1,uint _key2,bool _value) public {
+        uintUintBoolMapping[_key1][_key2]=_value;
+    }}
